94d9b52ccfcbe0e4d71f99705617bca73cdf5be3
heyvipul <vipulgirhestar@gmail.com>
2024-03-07 13:19:15 +0530

    pagination

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,46 +3,58 @@ import "./player.css"
 
 const Player = () => {
 
-  const [page,setPage] = useState(0);
-  const [videodata,setVideoData] = useState([])
-  const [loading,setLoading] = useState(false)
-   
-  async function getData(){
+  const [page, setPage] = useState(0);
+  const [videodata, setVideoData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  async function getData() {
     try {
-        const response  = await fetch(`https://internship-service.onrender.com/videos?page=${page}`)
-        const result = await response.json();
-        // console.log(result);
-        setVideoData(result.data.posts)
-        setLoading(true);
+      const response = await fetch(`https://internship-service.onrender.com/videos?page=${page}`)
+      const result = await response.json();
+      // console.log(result);
+      setVideoData(result.data.posts.splice(0,8))
+      setLoading(true);
 
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-  }  
+  }
 
-  useEffect(()=>{
+  useEffect(() => {
     getData();
-  },[page])
+  }, [page])
 
   console.log(videodata);
 
-  return (
-    <div className="wrapper">
 
-        <div className="all-cards">
-            {
-                videodata?.map(function(data,index){
-                    return <div key={index}>
-                        <img src={data.submission.thumbnail}  alt="user-poster" />
-                    </div>
-                })
-            }
+  return (
+
+    <div className="wrapper">
+    
+      {
+        loading ? <div className="all-cards">
+        {
+          videodata?.map(function (data, index) {
+            return <div key={index}>
+              <img src={data.submission.thumbnail} alt="user-poster" />
+            </div>
+          })
+        }
+        </div> : <div>
+            <h1 className="loading">Loading...</h1>
         </div>
-        <div>
-            <button className="sr.scroll-to-top" onClick={()=>setPage(page - 1)}>Prev</button>
-             <span>{page}</span>
-            <button  className="sr.scroll-to-top" onClick={()=>{setPage(page + 1); window.scrollTo(0,0)}}>Next</button>
-        </div>
+      }
+      
+
+      
+      {
+        loading ? <div className="btn-div">
+          <button disabled={page==0} className="sr.scroll-to-top" onClick={() => { setPage(page - 1); window.scrollTo(0, 0) }}>Prev</button>
+          <span>{page}</span>
+          <button className="sr.scroll-to-top" onClick={() => { setPage(page + 1); window.scrollTo(0, 0) }}>Next</button>
+        </div> :  ""
+      }
+
     </div>
   )
 }
