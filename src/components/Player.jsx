cd33efdcbc8bd3e5dd87326cb6b348af9f721715
heyvipul <vipulgirhestar@gmail.com>
2024-03-08 12:30:24 +0530

    hover-to-play

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import Skeleton from 'react-loading-skeleton';
 import "./player.css"
 
 const Player = () => {
@@ -7,13 +8,14 @@ const Player = () => {
   const [page, setPage] = useState(0);
   const [videodata, setVideoData] = useState([])
   const [loading, setLoading] = useState(false)
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   async function getData() {
     try {
       const response = await fetch(`https://internship-service.onrender.com/videos?page=${page}`)
       const result = await response.json();
       // console.log(result);
-      setVideoData(result.data.posts.splice(0,8))
+      setVideoData(result.data.posts.splice(0, 8))
       setLoading(true);
 
     } catch (error) {
@@ -25,37 +27,63 @@ const Player = () => {
     getData();
   }, [page])
 
-  console.log(videodata);
+  //function to play video on hover
+  const handleMouseEnter = index => {
+    setHoveredIndex(index);
+    const video = document.getElementById(`video-${index}`);
+    if (video) {
+      video.play();
+    }
+  };
+
+  //function to stop video after hover
+  const handleMouseLeave = index => {
+    setHoveredIndex(null);
+    const video = document.getElementById(`video-${index}`);
+    if (video) {
+      video.pause();
+      video.currentTime = 0;
+    }
+  };
+
+
 
 
   return (
 
     <div className="wrapper">
-    
+
       {
         loading ? <div className="all-cards">
-        {
-          videodata?.map(function (data, index) {
-            return <div key={index}>
-              <Link to={`singleplayer/${page}/${data.postId}`}>
-                <img src={data.submission.thumbnail} alt="user-poster" />
-              </Link> 
-            </div>
-          })
-        }
+          {
+            videodata?.map(function (data, index) {
+              return <div key={index}
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={() => handleMouseLeave(index)} >
+                <Link to={`singleplayer/${page}/${data.postId}`}>
+                  {/* <img src={data.submission.thumbnail} alt="user-poster" /> */}
+                  <video src={data.submission.mediaUrl}
+                    id={`video-${index}`}
+                    muted
+                    // autoPlay
+                    loop />
+                </Link>
+              </div>
+            })
+          }
         </div> : <div>
-            <h1 className="loading">Loading...</h1>
+          <h1 className="loading">Loading...</h1>
         </div>
       }
-      
 
-      
+
+
       {
         loading ? <div className="btn-div">
-          <button disabled={page==0} className="sr.scroll-to-top" onClick={() => { setPage(page - 1); window.scrollTo(0, 0) }}>Prev</button>
+          <button disabled={page == 0} className="sr.scroll-to-top" onClick={() => { setPage(page - 1); window.scrollTo(0, 0) }}>Prev</button>
           <span>{page}</span>
           <button className="sr.scroll-to-top" onClick={() => { setPage(page + 1); window.scrollTo(0, 0) }}>Next</button>
-        </div> :  ""
+        </div> : ""
       }
 
     </div>
